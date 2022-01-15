3488479b38fb0d2c8ce981e6c6f4ccb2471e76b9
sepp <sepp.acar@gmail.com>
2022-01-15 23:48:12 +0300

    -

--- singleasset_helper.py
+++ singleasset_helper.py
@@ -1,5 +1,6 @@
 import requests
 
+# TODO Fix all error handling, It is very messy right now, and maybe implement another API
 
 def get_collection(collection_slug):
     url = f"https://api.opensea.io/api/v1/collection/{collection_slug}"
@@ -36,14 +37,21 @@ def get_asset_rarity(token_id, collection_slug, trait_count):
     url = f'https://api.opensea.io/api/v1/assets?token_ids={token_id}&order_direction=desc&offset=0&limit=1&collection={collection_slug}'
     response = requests.get(url)
     rarity = 0
-    for trait_type in response.json()['assets'][0]['traits']:
-        rarity += 1 / (trait_type['trait_count'] / trait_count[trait_type['trait_type']])
+    try:
+        for trait_type in response.json()['assets'][0]['traits']:
+            rarity += 1 / (trait_type['trait_count'] / trait_count[trait_type['trait_type']])
+    except Exception:
+        print("Probably an API error")
     return rarity
 
 
 # Get asset image URL
 def get_asset_image(asset_json):
-    image = asset_json['assets'][0]['image_original_url']
+    try:
+        image = asset_json['assets'][0]['image_original_url']
+    except Exception:
+        print("Can't get image")
+        image = 'https://ipfs.io/ipfs/QmZ2ddtVUV1brVGjpq6vgrG6jEgEK3CqH19VURKzdwCSRf'
     if image.split(':')[0] == 'ipfs':
         image = 'http://ipfs.io/ipfs/' + image.split('//')[1]
     return image
@@ -60,8 +68,11 @@ def get_asset_data(collection_slug, token_id):
             'rarity': get_rarity(collection_slug, token_id), '_id': token_id, '_slug': collection_slug}
     traits = get_asset(collection_slug, token_id)
     data['traits'] = {}
-    for trait in traits['assets'][0]['traits']:
-        data['traits'][trait['trait_type']] = {}
-        data['traits'][trait['trait_type']] = trait['value']
+    try:
+        for trait in traits['assets'][0]['traits']:
+            data['traits'][trait['trait_type']] = {}
+            data['traits'][trait['trait_type']] = trait['value']
+    except Exception:
+        print("This is getting messy I wil lfix")
     return data
 
